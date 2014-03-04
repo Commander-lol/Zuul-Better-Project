06a365141725805a71bf1dbe886bc5d46445ede1
CosmicFrog <mortaljohnbat@yahoo.co.uk>
2014-03-04 14:55:57 +0000

    Documentation update: Inventory.java

--- state/Inventory.java
+++ state/Inventory.java
@@ -7,11 +7,22 @@ import util.Phraser;
 import entities.Item;
 
 /**
- * Write a description of class Inventory here.
- * 
- * @author (your name)
- * @version (a version number or a date)
- */
+* This class is part of the "World of Zuul" application. "World of Zuul" is a
+* very simple, text based adventure game
+*
+* This command hold information about the players inventory. The inventory
+* currently consists of the maximum and current weight and an array list of
+* item objects.
+* 
+* The way this is used is: Items can be removed and added to the list up to
+* the maximum weight when the name of the item is supplied. The printInventory
+* method can be called to print the items currently in the inventory if available.
+* Else it prints that the inventory is empty.
+*
+* @author Louis Capitanchik
+* @version 2014/03/04
+*/
+
 public class Inventory {
 	private float maxWeight, currentWeight;
 	private ArrayList<Item> items;
@@ -29,6 +40,7 @@ public class Inventory {
 	 * Attempts to add the item to the inventory, but will
 	 * not do so if that would increase the weight beyond
 	 * the defined maximum
+	 * 
 	 * @param newItem The item to put in the inventory
 	 * @return true if the item has successfully been added,
 	 * false if it is too heavy
@@ -41,9 +53,11 @@ public class Inventory {
 			return true;
 		}
 	}
+	
 	/**
 	 * Removes the first item in the inventory that has the given name
 	 * and returns it. Will return null if no item is found.
+	 * 
 	 * @param itemName The name of the item that will be removed
 	 * @return the Item whose name matches the given string
 	 */
@@ -61,6 +75,10 @@ public class Inventory {
 		return null;
 	}
 	
+	/**
+	 * If available, prints a list of the items in the inventory.
+	 * If the inventory is empty, prints a message informing the player.
+	 */
 	public void printInventory(){
 		if(items.size() == 0){
 			System.out.println("Your bag is empty");
