53c19edff0d9822e5410d27006b48df1d3ae59aa
jomooftheshire <joshua_f_mulcock@hotmail.com>
2014-03-04 14:03:48 +0000

    Update NPC.java
    Added a constructor. Work needs to be done on the currentRoom problem. Commonets need to be added. Methods added are: getItemNeeded,printSpeech, getItemHeld.

--- entities/NPC.java
+++ entities/NPC.java
@@ -1,5 +1,35 @@
 package entities;
+import entities.Item;
 
 public class NPC {
-	
+    
+private String name;
+private Item itemHeld;
+private Item itemNeeded;
+private String speech;
+private Room currentRoom;
+    
+public NPC (String npcName, Item needed, Item held, String npcSpeech, Room npcRoom){
+    name = npcName;
+    itemNeeded = needed;
+    itemHeld = held;
+    speech = npcSpeech;
+    currentRoom = npcRoom
 }
+
+public Item getItemNeeded(){
+    return itemNeeded;
+}
+
+public void printSpeech(){
+    System.out.println(speech);
+}
+
+public Item getItemHeld(){
+    return itemHeld;
+}
+
+public Room getCurrentRoom(){
+   
+}
+
